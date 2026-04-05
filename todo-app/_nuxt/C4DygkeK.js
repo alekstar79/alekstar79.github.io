64c4443232b9aeac2569eeed0f614d32d02c41f8
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-04-05 23:48:20 +0400

    todo-app have been added

---
+++ todo-app/_nuxt/C4DygkeK.js
@@ -0,0 +1,1 @@
+const a=[{id:1,email:"admin@test.com",password:"123456",role:"admin",name:"Админ",avatar:"👑"},{id:2,email:"user@test.com",password:"123456",role:"user",name:"Иван Иванов",avatar:"👤"}];export{a as u};
