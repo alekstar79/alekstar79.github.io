8d2d2bfc3e0e666726715d65ef6fbece08f9de11
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-04-18 21:45:03 +0400

    miro-boards redirecting fix

---
+++ miro-boards/assets/board.page-Bc9a467p.js
@@ -0,0 +1,1 @@
+import{o as r,j as a}from"./index-DPpuK8rJ.js";function s(){const o=r();return a.jsxs("div",{children:["Board page ",o.boardId]})}const t=s;export{t as Component};
--- miro-boards/assets/board.page-Ca3b8t7j.js
+++
@@ -1,1 +0,0 @@
-import{o as r,j as a}from"./index-Bs002-nk.js";function s(){const o=r();return a.jsxs("div",{children:["Board page ",o.boardId]})}const t=s;export{t as Component};
