a9e451c25da9dbe76058508bbc66f8b5836ca9cb
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-04-19 00:11:03 +0400

    miro-boards github:fix

--- miro-boards/assets/board.page-CGj8AfpM.js
+++
@@ -1,1 +0,0 @@
-import{o as r,j as a}from"./index-Cq_DqJgZ.js";function s(){const o=r();return a.jsxs("div",{children:["Board page ",o.boardId]})}const t=s;export{t as Component};
---
+++ miro-boards/assets/board.page-Cr9HkyCl.js
@@ -0,0 +1,1 @@
+import{o as r,j as a}from"./index-BGARCb-K.js";function s(){const o=r();return a.jsxs("div",{children:["Board page ",o.boardId]})}const t=s;export{t as Component};
